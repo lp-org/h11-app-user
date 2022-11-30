74ef356eed0738f5326e73aa5aba2f43e8ab3459
anchong.lim <anchong.lim@kinchotech.com>
2022-11-30 09:26:21 +0800

    fix: env

---
+++ src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  baseUrl: "http://47.254.247.170:9090/api",
+};
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { environment } from "environment/environment";
 // Set config defaults when creating the instance
 
 export const request = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: environment.baseUrl,
 });
