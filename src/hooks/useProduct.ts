f1f03b16fc93301d72a22c7e516e699321a916e0
anchong.lim <anchong.lim@kinchotech.com>
2022-12-07 17:03:31 +0800

    fix: remove unused store

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,10 +1,7 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { environment } from "environment/environment";
-import { useHistory } from "react-router";
-import { useProductStore } from "store/useProductStore";
-import { AddProductProps, Product } from "types/product";
+import { useQuery } from "@tanstack/react-query";
+
+import { Product } from "types/product";
 import { request } from "utils/request";
-import { usePopUpMessage } from "./notification";
 
 export function useProductList() {
   return useQuery({
@@ -12,74 +9,6 @@ export function useProductList() {
     queryFn: async (): Promise<Product[]> =>
       (await request.get("/product/showall")).data.data,
   });
-}
-
-export function useAddProduct() {
-  const history = useHistory();
-  const popUpMsg = usePopUpMessage();
-  const uploadImage = useUploadProductImage();
-  const dispatchClearTempProductSetup = useProductStore(
-    (state) => state.clearTempProductSetup
-  );
-  return useMutation(
-    async (payload: AddProductProps) => {
-      const res = await request.post("/product/add", payload);
-      if (res.data.code === 200) {
-        if (payload.prd_image?.substring(0, 4) === "blob") {
-          uploadImage.mutate({
-            id: payload.prd_code,
-            filebody: payload.prd_image,
-          });
-          //@ts-ignore
-          delete payload["prd_image"];
-        }
-        popUpMsg("Product have been successfully created!", "success");
-        return res;
-      } else {
-        popUpMsg(res.data.message, "error");
-        throw new Error(res.data.message);
-      }
-    },
-    {
-      onSuccess: () => {
-        history.push("/product");
-        dispatchClearTempProductSetup();
-      },
-    }
-  );
-}
-
-export function useEditProduct() {
-  const history = useHistory();
-  const queryClient = useQueryClient();
-  const popUpMsg = usePopUpMessage();
-  const uploadImage = useUploadProductImage();
-  return useMutation(
-    async ({ id, payload }: { id: string; payload: AddProductProps }) => {
-      if (payload.prd_image?.substring(0, 4) === "blob") {
-        uploadImage.mutate({ id, filebody: payload.prd_image });
-        //@ts-ignore
-        delete payload["prd_image"];
-      }
-
-      const res = await request.post(`/product/edit/${id}`, payload);
-
-      if (res.data.code === 200) {
-        queryClient.invalidateQueries(["products"]);
-        queryClient.invalidateQueries(["product", id]);
-        popUpMsg("Product have been successfully updated!", "success");
-        return res;
-      } else {
-        popUpMsg(res.data.message, "error");
-        throw new Error(res.data.message);
-      }
-    },
-    {
-      onSuccess: () => {
-        history.replace("/product");
-      },
-    }
-  );
 }
 
 export function useGetProductById(code: string) {
@@ -95,25 +24,4 @@ export function useGetProductId() {
     queryFn: async (): Promise<string> =>
       (await request.get(`/product/getproductcode`)).data.data.New_Product_Code,
   });
-}
-
-export function useUploadProductImage() {
-  const { fileLimit } = environment;
-  return useMutation(
-    async ({ id, filebody }: { id: string; filebody: string }) => {
-      if (filebody) {
-        const fileExt = "png";
-        const filename = `${Math.random()}.${fileExt}`;
-
-        const res = await fetch(filebody);
-        const blob = await res.blob();
-        if (blob.size > fileLimit) {
-          throw new Error("File size exceeded");
-        }
-        const formData = new FormData();
-        formData.append("multiPartFile", blob, filename);
-        return request.post(`/product/upload/${id}`, formData);
-      }
-    }
-  );
 }
